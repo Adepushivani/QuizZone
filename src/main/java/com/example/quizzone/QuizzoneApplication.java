87ed044ea041d12baa946535080c14379bf83fad
Adepushivani <adepushivani2002@gmail.com>
2023-07-12 09:51:34 +0530

    First Commit for BackEnd Project

---
+++ src/main/java/com/example/quizzone/QuizzoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzoneApplication.class, args);
+	}
+
+}
